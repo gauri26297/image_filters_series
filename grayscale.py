d52a75ac9ee9373b845442da0ad287286906fe97
gauri26297 <gauri26297@gmail.com>
2023-02-15 23:30:41 +0530

    default BGR to Grayscale conversion and display

--- grayscale.py
+++ grayscale.py
@@ -1,2 +1,26 @@
+#imports
 import cv2 as cv
 
+#capture the image stream from available camera
+cap = cv.VideoCapture(0)
+
+#make sure camera permissions are given
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    #Capture frame
+    ret, frame = cap.read()
+    #if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame, Exiting ...")
+        break
+    
+    #actual filter works here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    #display the filter
+    cv.imshow("grayscale",gray)
+    cv.waitKey(1)
+cv.destroyAllWindows()
